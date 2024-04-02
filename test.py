4522c5ce742672ec95eef9acbcbc45f221041ac3
Mochendra <hendraadmaja946@gmail.com>
2024-04-02 20:58:36 +0700

    First Commit

---
+++ test.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+today = datetime.now()
+# print(today)
+date_time = today.strftime('%Y-%m-%d-%H-%M-%S')
+
+print(date_time)
+
+
+# =====================================================
+# first_name = 'Hendra'
+# last_name = 'Admaja'
+# # full_name = first_name + ' ' + last_name
+# full_name = f'{first_name} {last_name}'
+# print(full_name)
+# ===================================================== 
